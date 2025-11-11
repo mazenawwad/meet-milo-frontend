76a981d23a42ce743830a8210c224ace4ccbbed4
Mazen <mazenawwad2003@gmail.com>
2025-11-11 20:52:14 +0200

    design | header - reworked the header design by giving it same color as background - defined separate header button component and data class for scalability - give header title own glow animation on hover - hide buttons on smaller screens and recenter title, to show side menu on the side - give wobble animation to the header buttons on hover

---
+++ meet-milo-frontend/app/constants/HeaderButtons.ts
@@ -0,0 +1,19 @@
+export type HeaderButton = {
+  label: string;
+  path: string;
+};
+
+export const HeaderButtons: HeaderButton[] = [
+  {
+    label: "Blog",
+    path: "/blog",
+  },
+  {
+    label: "Store",
+    path: "/store",
+  },
+  {
+    label: "About",
+    path: "/about",
+  },
+];
